ac666bbb6dfe4a4138d0ff56c5c34f95c387c11b
Han Wang <hyw2@cornell.edu>
2019-08-30 17:27:23 -0400

    Add initial file structure

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+});
