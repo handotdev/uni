75927ce1e321db564a0cd511701ff45956762ee6
Han Wang <hyw2@cornell.edu>
2019-08-30 17:59:44 -0400

    Add MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const app = express();
 
-app.get('/api', (req, res) => {
-    res.send("Hello")
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://root:6atKluiQ93uD7tHW@cluster0-iyhxj.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const dbName = 'uni';
+
+const apiKey = '422D';
+
+app.get(`/api/designers`, (req, res) => {
+    const key = req.query.key;
+
+    if (key === apiKey) {
+        client.connect(err => {
+            if (err) throw err;
+
+            const db = client.db(dbName);
+
+            findDocuments(db, docs => {
+                res.send(docs);
+                client.close();
+            });
+        })
+    }
+    res.end();
+
+    const findDocuments = (db, callback) => {
+        // Get the documents collection
+        const collection = db.collection('documents');
+        // Find some documents
+        collection.find({}).toArray(function(err, docs) {
+          if (err) throw err;
+          callback(docs);
+        });
+      }
 });
 
 app.get('/*', (req, res) => {
