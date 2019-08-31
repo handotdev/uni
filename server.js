1a3b07be65b2cccb3d92e2adfde4fbdac443196e
Han Wang <hyw2@cornell.edu>
2019-08-31 14:35:41 -0400

    Add initial design draft

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const dbName = 'uni';
 
 const apiKey = '422D';
 
+app.use(express.static(__dirname + '/public'));
+
 app.get(`/api/designers`, (req, res) => {
     const key = req.query.key;
 
@@ -22,8 +24,19 @@ app.get(`/api/designers`, (req, res) => {
                 client.close();
             });
         })
+    } else {
+        res.end();
     }
-    res.end();
+
+    const insertDocuments = (db, callback) => {
+        // Get the documents collection
+        const collection = db.collection('documents');
+        // Insert some documents
+        collection.insertMany([{a : 1}, {a : 2}, {a : 3}],(err, result) => {
+          console.log(result);
+          callback(result);
+        });
+      }
 
     const findDocuments = (db, callback) => {
         // Get the documents collection
@@ -36,11 +49,7 @@ app.get(`/api/designers`, (req, res) => {
       }
 });
 
-app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-});
-
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 });
